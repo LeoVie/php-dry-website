10388f048c352282d8fb7f2fe31911a0c06960a5
Leo Viezens <leo.viezens@web.de>
2022-01-31 20:02:45 +0100

    Add index page

--- public/index.php
+++ public/index.php
@@ -1,9 +1,16 @@
 <?php
 declare(strict_types=1);
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../template/');
-$twig = new \Twig\Environment($loader, [
-    'cache' => '/path/to/compilation_cache',
+$loader = new FilesystemLoader(__DIR__ . '/../template/');
+$twig = new Environment($loader, [
+    // 'cache' => __DIR__ . '/../cache/compilation/',
+    'cache' => false,
 ]);
+
+$template = $twig->load('index.twig');
+echo $template->render();
